3e384f3e36cfab86b8fc7566fc6d94fc5f82d390
Sabbir Ahmmed <sabbir.cse@yahoo.com>
2021-05-31 23:45:48 +0600

    Enter Details page done

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './Dashboard.css'
+import question from '../../images/Group 90.png'
+import info from '../../images/Group 91.png'
+
+const Dashboard = () => {
+
+    return (
+        <section>
+            <div className="row">
+                <form className="col details-container">
+                    <div className="d-flex justify-content-between que-img">
+                        <h5>Enter Details</h5>
+                        <img src={question} alt="" className="d-block" />
+                    </div>
+
+                    <div className="textarea-container">
+                        <textarea className="form-control" name="" id="" cols="" rows="5" placeholder="Product Description"></textarea>
+                    </div>
+
+                    <div className="progressBar">
+                        <div></div>
+                    </div>
+                    <p className="status">Poor</p>
+
+                    <div className="d-flex align-items-center">
+                        <input type="text" className="form-control mt-4 w-100 d-block f-size" placeholder="Keyword (3-5 Remembered, Comma Separate)"/>
+                        <img src={info} alt="info" className="info-img d-block" />
+                    </div>
+                    <input type="text" className="form-control mt-4 f-size" placeholder="Keyword to Avoid (3-5 Remembered, Comma Separate)"/>
+
+                    <input type="submit" value="Generate" className="mt-5 d-block mx-auto submit-form btn"/>
+                </form>
+
+                <div className="col">
+                    <p>select all</p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Dashboard;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Dashboard from '../Dashboard/Dashboard';
 import Header from '../Header/Header';
 
 const Home = () => {
     return (
-        <main>
-            <Header/>
+        <main style={{margin:'10px 20px'}}>
+            <Header />
+            <Dashboard />
         </main>
     );
 };
